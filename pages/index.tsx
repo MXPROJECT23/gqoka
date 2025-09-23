81877817e438600f2eabea0d8de39ad57deff53a
MXPROJECT23 <gqoka23@gmail.com>
2025-09-23 13:58:15 +0200

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      background: '#111',
+      color: '#fff',
+      fontFamily: 'sans-serif',
+      flexDirection: 'column'
+    }}>
+      <h1>🚧 Gqoka en construction 🚧</h1>
+      <p>Revenez bientôt pour découvrir la plateforme.</p>
+    </div>
+  )
+}
