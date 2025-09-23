c73ee911beae7878898ff54a09c5c23f8ca354bc
MXPROJECT23 <gqoka23@gmail.com>
2025-09-23 23:03:46 +0200

    Create _document.tsx

---
+++ pages/_document.tsx
@@ -0,0 +1,3 @@
+<meta property="og:title" content="GQOKA – Votre styliste IA" />
+<meta property="og:description" content="Des looks réellement portables, basés sur vos pièces." />
+<meta property="og:type" content="website" />
