aacb2ec562d832be028e42d49fc383a8e64451a7
MXPROJECT23 <gqoka23@gmail.com>
2025-09-24 01:20:01 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,17 @@ import AnnaFloatingButton from "../components/AnnaFloatingButton";
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col bg-gray-50">
+      {/* Header fixé en haut */}
       <Header />
+
+      {/* Section principale */}
       <main className="flex-1 flex items-center justify-center">
         <Hero />
       </main>
+
+      {/* Bouton flottant Anna */}
       <AnnaFloatingButton />
     </div>
   );
 }
+
