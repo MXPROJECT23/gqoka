b6e3f69546cb8d4e8f1268dac191836e9fbd98f8
MXPROJECT23 <gqoka23@gmail.com>
2025-09-23 22:58:24 +0200

    Update signup.tsx

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,3 +1,6 @@
+import AnnaFloatingButton from "../components/AnnaFloatingButton";
+// ...dans le JSX :
+<AnnaFloatingButton />
 import { useState } from "react";
 import { supabase } from "../lib/supabaseClient";
 
