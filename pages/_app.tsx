a61c71b4d789633b4e4f0a33c075775417ae2951
MXPROJECT23 <gqoka23@gmail.com>
2025-10-01 22:25:53 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import dynamic from "next/dynamic";
+
+const RgpdBanner = dynamic(() => import("../components/RgpdBanner"), { ssr: false });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <RgpdBanner />
+    </>
+  );
 }
