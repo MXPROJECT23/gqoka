16ed840f5f08991526d80ca1d01b279b5f4f37a3
MXPROJECT23 <gqoka23@gmail.com>
2025-10-03 00:15:29 +0200

    Delete components directory

--- components/AnnaBadge.tsx
+++
@@ -1,7 +0,0 @@
-export default function AnnaBadge() {
-  return (
-    <span className="inline-flex items-center gap-1 px-2 py-1 rounded-full text-xs font-medium bg-black text-white">
-      ✔ Validé par Anna
-    </span>
-  );
-}
