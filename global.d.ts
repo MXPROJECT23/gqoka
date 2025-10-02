6100e8276a11b6a83c64f1983aca95cb20aa83ae
MXPROJECT23 <gqoka23@gmail.com>
2025-10-02 17:33:25 +0200

    Create global.d.ts

---
+++ global.d.ts
@@ -0,0 +1,12 @@
+// global.d.ts
+declare namespace JSX {
+  interface IntrinsicElements {
+    "df-messenger": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+      intent?: string;
+      "chat-title"?: string;
+      "agent-id"?: string;
+      "language-code"?: string;
+      "chat-icon"?: string;
+    };
+  }
+}
