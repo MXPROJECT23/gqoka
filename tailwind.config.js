310240584cfedb6fc6aed8dd539143721fcbfe35
MXPROJECT23 <gqoka23@gmail.com>
2025-10-02 16:17:07 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
-  theme: {
-    extend: {
-      colors: { brand: { DEFAULT: "#0f0f0f" } },
-      borderRadius: { xl: "1rem", "2xl": "1.25rem" }
-    }
-  },
+  theme: { extend: {} },
   plugins: []
 };
+
 
