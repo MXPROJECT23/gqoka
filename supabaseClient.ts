2654715368abef89da86fc6a8720ee4ff32489ae
MXPROJECT23 <gqoka23@gmail.com>
2025-10-03 21:40:33 +0200

    Update supabaseClient.ts

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,3 +1,5 @@
+console.log("DEBUG Supabase URL:", import.meta.env.VITE_SUPABASE_URL)
+console.log("DEBUG Supabase Key:", import.meta.env.VITE_SUPABASE_ANON_KEY ? "LOADED" : "MISSING")
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string
