83bab89706542efce6aac0d23b5612e0a3ca8a0d
MXPROJECT23 <gqoka23@gmail.com>
2025-10-04 19:46:52 +0200

    Add files via upload

--- src/components/AutoCompleteInput.tsx
+++ src/components/AutoCompleteInput.tsx
@@ -1,1 +1,17 @@
+import { useId } from 'react';
 
+type Props = React.InputHTMLAttributes<HTMLInputElement> & {
+  options: string[]
+};
+
+export default function AutoCompleteInput({ options, ...rest }: Props) {
+  const id = useId();
+  return (
+    <>
+      <input list={id} {...rest} className={"border px-3 py-2 rounded w-full " + (rest.className ?? '')} />
+      <datalist id={id}>
+        {options.map(o => <option key={o} value={o} />)}
+      </datalist>
+    </>
+  );
+}
