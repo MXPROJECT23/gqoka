ad6a2cb5d09a8fb2422d43e24b463b6d9ace9c1b
MXPROJECT23 <gqoka23@gmail.com>
2025-10-04 17:28:35 +0200

    Delete netlify/functions directory

--- netlify/functions/weather.ts
+++
@@ -1,9 +0,0 @@
-import type { Handler } from '@netlify/functions'
-
-export const handler: Handler = async (event) => {
-  const { lat = '48.8566', lon = '2.3522' } = event.queryStringParameters || {}
-  const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m,precipitation&current_weather=true`
-  const r = await fetch(url)
-  const data = await r.json()
-  return { statusCode: 200, body: JSON.stringify(data) }
-}
