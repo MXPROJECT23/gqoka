751b4f01f6288babf7d26cfb2df4045fc67dd045
MXPROJECT23 <gqoka23@gmail.com>
2025-10-04 19:22:10 +0200

    Delete lists.ts

--- lists.ts
+++
@@ -1,9 +0,0 @@
-export const BRANDS = [
-  'Nike','Adidas','Zara','H&M','Uniqlo','Levi's','Louis Vuitton','Gucci','Prada','Balenciaga','New Balance','Asics','Reebok'
-];
-
-export const SIZES = ['XS','S','M','L','XL','XXL','36','38','40','42','44'];
-
-export const CATEGORIES = ['T-shirt','Chemise','Pantalon','Jean','Robe','Jupe','Short','Veste','Manteau','Sweat','Pull','Chaussures','Accessoire'];
-
-export const COLORS = ['Noir','Blanc','Gris','Bleu','Rouge','Vert','Jaune','Rose','Beige','Marron','Violet'];
