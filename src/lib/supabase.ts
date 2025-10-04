c3ad1570f8244e740ded4e2b3bc7a6ec502bc823
MXPROJECT23 <gqoka23@gmail.com>
2025-10-04 16:48:43 +0200

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,9 +1,5 @@
-// src/lib/supabase.ts
 import { createClient } from '@supabase/supabase-js';
-
-// OK côté front (RLS protège). Remplace par tes valeurs si besoin.
-const SUPABASE_URL = 'https://obrfeydglsdspqjhcsvx.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9icmZleWRnbHNkc3Bxamhjc3Z4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc0NjA2NDcsImV4cCI6MjA3MzAzNjY0N30.p6jbyZ4AFu75xCCG-YQgc6k25cuKwwq_mkaGnIv027c';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-export default supabase;
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL as string,
+  import.meta.env.VITE_SUPABASE_ANON_KEY as string
+);
